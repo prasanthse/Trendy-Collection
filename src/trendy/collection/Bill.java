e1e03ee28e8d79a6ab54c507c3983682262086b9
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-23 19:32:18 +0530

    bill class created

---
+++ src/trendy/collection/Bill.java
@@ -0,0 +1,22 @@
+package trendy.collection;
+
+public class Bill {
+    
+    public void billPrinting(){
+        
+        System.out.println("--------------------_ _ _ _ _ TRENDY _ _ _ _ _--------------------");
+        System.out.println("_ _ _ _ _ _ _ _ _ COLLECTION _ _ _ _ _ _ _ _ _");
+//        System.out.println("\t\t " + );
+//        System.out.println("\t\t " + );
+//        System.out.println("CASHIER: " + );
+//        System.out.println("UNIT: " + + "\t\tINVOICE: " + );
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+        
+        
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+        //System.out.println("SUB TOTAL                                " + );
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+        System.out.println("\t\tThank You Come Again\n\t\tEXCHANGES POSSIBLE\n\t\tWITHIN 7 DAYS\n*Party Wear, Belt, Undergarments, Toys, Gift Items, Cosmetic\nItems, Perfumes Are No Exchangeable* ");
+        System.out.println("_ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n_ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+    }
+}
