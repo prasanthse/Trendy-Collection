da50af52f495709c83dbad315ea490c9f7f84691
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-27 20:13:34 +0530

    Connection class finished

--- src/trendy/collection/Database/Connection.java
+++ src/trendy/collection/Database/Connection.java
@@ -1,6 +1,8 @@
 package trendy.collection.Database;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Connection {
     
@@ -9,8 +11,59 @@ public class Connection {
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
 
-    Class.forName("com.mysql.jdbc.Driver");  
-    connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/trendycollection","root","");   
-    Statement stmt=con.createStatement();  
-    ResultSet rs=stmt.executeQuery("select * from emp");   
+    public Connection(){
+        try {
+            makeConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Connection(Connection connect, Statement statement, PreparedStatement preparedStatement, ResultSet resultSet){
+        this.connect = connect;
+        this.statement = statement;
+        this.preparedStatement = preparedStatement;
+        this.resultSet = resultSet;
+    }
+    
+    public Connection getConnect() {
+        return connect;
+    }
+
+    public void setConnect(Connection connect) {
+        this.connect = connect;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Statement statement) {
+        this.statement = statement;
+    }
+
+    public PreparedStatement getPreparedStatement() {
+        return preparedStatement;
+    }
+
+    public void setPreparedStatement(PreparedStatement preparedStatement) {
+        this.preparedStatement = preparedStatement;
+    }
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+    public void setResultSet(ResultSet resultSet) {
+        this.resultSet = resultSet;
+    }
+    
+    public void makeConnection() throws SQLException{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            setConnect((Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/trendycollection","root","")); 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
